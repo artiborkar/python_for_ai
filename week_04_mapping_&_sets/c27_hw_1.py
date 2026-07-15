a760e239c3ae742d33e02f4e6f7ab3cafd26b202
artiborkar <borkararti207@gmail.com>
2026-07-15 17:45:26 +0530

    create a homework files

---
+++ week_04_mapping_&_sets/c27_hw_1.py
@@ -0,0 +1,16 @@
+
+# class 27
+# homework
+
+# 3 dosto ka phone book banao (naam: number) aur .items() se sab print karo.
+
+
+print("=========Q1=============")
+
+phone_book = {
+                "Rohini" : 7777777777 ,
+                "Shreya" : 5555555555 ,
+                "Diksha" : 2222222222
+}
+
+print(phone_book.items())
