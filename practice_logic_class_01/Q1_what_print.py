36ad2a637f9c5385f619d0df41be7d111e9298e6
artiborkar <borkararti207@gmail.com>
2026-07-22 12:14:55 +0530

    some files are creted or some file are modifys

--- practice_logic_class_01/Q1_what_print.py
+++ practice_logic_class_01/Q1_what_print.py
@@ -1,6 +1,6 @@
 # Q1
 # EN: Trace x step by step. What prints? HI: x ko step-by-step trace karo. Kya print hoga?
-# build a trace table on paper, predict
+# build a trace table on paper, predicts
 
 x = 5
 x = x + 3
